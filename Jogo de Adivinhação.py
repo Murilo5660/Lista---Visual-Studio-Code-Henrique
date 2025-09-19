ae4eaec6d57e915e5785966d7a2a77ec3fb3bb6b
Murilo <murilo.stresser@escola.pr.gov.br>
2025-09-19 10:32:05 -0300

    Add files via upload

---
+++ Jogo de Adivinhação.py
@@ -0,0 +1,12 @@
+import random
+
+random.randint(1,100)
+
+while palpite =! numro_secreto
+
+palpite = int (input("De um palpide de qual número estou pensando"))
+
+if palpite < numero_secreto:
+    print ("É maior!")
+else:
+    print ("É menor!")
